611cfa349eb1298578d848d62f12ed76d46f84ce
Salman Deshmukh <msalmand1@gmail.com>
2026-05-23 11:54:48 +0530

    connect frontend with live backend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+)
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
